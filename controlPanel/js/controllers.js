02af77aa01fe1a57f4e82b28308dbaeae75bfebb
Bryan Nissen <bryan@webizly.com>
2013-04-13 14:58:46 -0400

    Added USer selection

--- controlPanel/js/controllers.js
+++ controlPanel/js/controllers.js
@@ -6,7 +6,9 @@ function UserList($scope, $http, User){
 	/*$http.get("http://web.njit.edu/~bcn3/IT332Project/controlPanel/php/retrieveData.php").success(function(data){
 		$scope.users = data;
 	});*/
-	$scope.users = User.query();
+	$scope.users = User.query(function(user){
+		$scope.selected = user[0];
+	});
 
 	$scope.orderProp = 'uid';
 	$scope.reverse = true;
@@ -19,6 +21,12 @@ function UserList($scope, $http, User){
 		}else if ($scope.orderProp === 'credit_card'){
 			$scope.reverse = true;
 		}
+	}
+
+	$scope.selectUser = function(user) {
+		$('#' + $scope.selected.uid).removeClass('selected');
+	    $scope.selected = user;
+	    $('#' + user.uid).addClass('selected');
 	}
 }
 
