10e5a098c8bfbf2ec32116918cafb06dae336694
Dan Michalak <djm.metalbane@gmail.com>
2013-04-17 14:46:31 -0400

    php testing

--- registration.php
+++ registration.php
@@ -60,6 +60,10 @@
 	
 	$count = 0;
 	
+	$nameError = FALSE;
+	$emailError = FALSE;
+	$questionError = FALSE;
+	
 	$array = array(
 		$creditCard,
 		$amazon,
@@ -109,8 +113,6 @@
 		$wordpress
 	);
 	
-	//echo count($array);
-	
 	for($i = 0; $i < count($array); $i++) {
 		if($array[$i] === "on") {
 			$array[$i] = "true";
@@ -118,7 +120,6 @@
 		}else{
 			$array[$i] = "false";
 		}
-		//echo "Array[".$i."] = ".$array[$i]."<br>";
 	}
 	
 	// Create connection
@@ -126,8 +127,12 @@
 	
 	// Check connection
 	if (mysqli_connect_errno($con)) {
-		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		echo "Registration error!";
+		echo "Failed to connect to database: " . mysqli_connect_error();
 	} else {
+		
+		
+		/*
 		echo $username . "<br>";
 		echo $password . "<br>";
 		echo $email . "<br>";
@@ -140,123 +145,151 @@
 		for($i = 0; $i < count($array); $i++) {
 			echo $array[$i] . "<br>";
 		}
+		*/
 		
+		$accounts = mysqli_query($con,"SELECT username FROM synergy_web_services");
 		
-		mysqli_query($con, "INSERT INTO synergy_web_services (
-			username,
-			password,
-			email,
-			question1,
-			answer1,
-			question2,
-			answer2,
-			question3,
-			answer3,
-			credit_card,
-			amazon,
-			aol,
-			apple,
-			battlenet,
-			bebo,
-			blogspot,
-			classmates_dot_com,
-			comcast,
-			delicious,
-			deviantart,
-			digg,
-			ebay,
-			facebook,
-			flickr,
-			flixster,
-			foursquare,
-			gaia_online,
-			google,
-			hotmail,
-			instagram,
-			imgur,
-			lastfm,
-			league_of_legends,
-			linkedin,
-			livejournal,
-			microsoft,
-			mylife,
-			myspace,
-			newegg,
-			newsvine,
-			optimum,
-			origin,
-			paypal,
-			planetside,
-			playstation,
-			reddit,
-			second_life,
-			steam,
-			stumbleupon,
-			tumblr,
-			twitter,
-			yahoo,
-			youtube,
-			world_of_warcraft,
-			wordpress,
-			num_services
-		) VALUES (
-			'$username',
-			'$password',
-			'$email',
-			'$question1',
-			'$answer1',
-			'$question2',
-			'$answer2',
-			'$question3',
-			'$answer3',
-			'$array[0]',
-			'$array[1]',
-			'$array[2]',
-			'$array[3]',
-			'$array[4]',
-			'$array[5]',
-			'$array[6]',
-			'$array[7]',
-			'$array[8]',
-			'$array[9]',
-			'$array[10]',
-			'$array[11]',
-			'$array[12]',
-			'$array[13]',
-			'$array[14]',
-			'$array[15]',
-			'$array[16]',
-			'$array[17]',
-			'$array[18]',
-			'$array[19]',
-			'$array[20]',
-			'$array[21]',
-			'$array[22]',
-			'$array[23]',
-			'$array[24]',
-			'$array[25]',
-			'$array[26]',
-			'$array[27]',
-			'$array[28]',
-			'$array[29]',
-			'$array[30]',
-			'$array[31]',
-			'$array[32]',
-			'$array[33]',
-			'$array[34]',
-			'$array[35]',
-			'$array[36]',
-			'$array[37]',
-			'$array[38]',
-			'$array[39]',
-			'$array[40]',
-			'$array[41]',
-			'$array[42]',
-			'$array[43]',
-			'$array[44]',
-			'$array[45]',
-			'$count'
-		)");
+		while($row = mysqli_fetch_array($accounts)) {
+			if($username == $row['username']) {
+				$nameError = TRUE;
+			}
+			if($email == $row['email']) {
+				$emailError = TRUE;
+			}
+		}
+		
+		if(($question1 == $question2) || ($question2 == $question3) || ($question3 == $question1)) {
+			$questionError = TRUE;
+		}
+		
+		if(($nameError == TRUE) || ($emailError == TRUE) || ($questionError == TRUE)) {
+			echo "Registration Error!";
+			if($nameError == TRUE) {
+				echo "That username already exists in our database";
+			}
+			if($emailError == TRUE) {
+				echo "That email account already exists in our database";
+			}
+			if($questionError == TRUE) {
+				echo "You must choose three <b>different</b> security questions";
+			}
+		} else {
+			mysqli_query($con, "INSERT INTO synergy_web_services (
+				username,
+				password,
+				email,
+				question1,
+				answer1,
+				question2,
+				answer2,
+				question3,
+				answer3,
+				credit_card,
+				amazon,
+				aol,
+				apple,
+				battlenet,
+				bebo,
+				blogspot,
+				classmates_dot_com,
+				comcast,
+				delicious,
+				deviantart,
+				digg,
+				ebay,
+				facebook,
+				flickr,
+				flixster,
+				foursquare,
+				gaia_online,
+				google,
+				hotmail,
+				instagram,
+				imgur,
+				lastfm,
+				league_of_legends,
+				linkedin,
+				livejournal,
+				microsoft,
+				mylife,
+				myspace,
+				newegg,
+				newsvine,
+				optimum,
+				origin,
+				paypal,
+				planetside,
+				playstation,
+				reddit,
+				second_life,
+				steam,
+				stumbleupon,
+				tumblr,
+				twitter,
+				yahoo,
+				youtube,
+				world_of_warcraft,
+				wordpress,
+				num_services
+			) VALUES (
+				'$username',
+				'$password',
+				'$email',
+				'$question1',
+				'$answer1',
+				'$question2',
+				'$answer2',
+				'$question3',
+				'$answer3',
+				'$array[0]',
+				'$array[1]',
+				'$array[2]',
+				'$array[3]',
+				'$array[4]',
+				'$array[5]',
+				'$array[6]',
+				'$array[7]',
+				'$array[8]',
+				'$array[9]',
+				'$array[10]',
+				'$array[11]',
+				'$array[12]',
+				'$array[13]',
+				'$array[14]',
+				'$array[15]',
+				'$array[16]',
+				'$array[17]',
+				'$array[18]',
+				'$array[19]',
+				'$array[20]',
+				'$array[21]',
+				'$array[22]',
+				'$array[23]',
+				'$array[24]',
+				'$array[25]',
+				'$array[26]',
+				'$array[27]',
+				'$array[28]',
+				'$array[29]',
+				'$array[30]',
+				'$array[31]',
+				'$array[32]',
+				'$array[33]',
+				'$array[34]',
+				'$array[35]',
+				'$array[36]',
+				'$array[37]',
+				'$array[38]',
+				'$array[39]',
+				'$array[40]',
+				'$array[41]',
+				'$array[42]',
+				'$array[43]',
+				'$array[44]',
+				'$array[45]',
+				'$count'
+			)");
+		}
 	}
 	
 ?>
