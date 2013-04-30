06ba861f57115fefe60521d26c1fbc8c3c8aae84
Dan Michalak <djm.metalbane@gmail.com>
2013-04-30 02:09:16 -0400

    control panel fix

--- controlPanel/login.php
+++ controlPanel/login.php
@@ -3,6 +3,8 @@
 $name = $_POST["username"];
 $pass = $_POST["password"];
 
+$con = mysqli_connect("sql.njit.edu", "djm28_proj", "6l28pgnN", "djm28_proj");
 
+$info = mysqli_query($con, "SELECT * FROM synergy_web_services");
 
 ?>
